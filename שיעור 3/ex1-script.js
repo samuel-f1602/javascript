967477943bf4fa28aafa4f9973dd6974b8473764
samuel-f1602 <samuelfrau20@gmail.com>
2024-11-09 20:25:49 +0200

    ex

---
+++ שיעור 3/ex1-script.js
@@ -0,0 +1,15 @@
+const uName = prompt('שלום, מדברה אנדרואיד, מה השם בבקשה?');
+
+let many;
+
+function aloF() {
+    const alone = prompt('יש לי שאלה, ' + uName + ',אתה מגיע לבד?');
+
+    if (alone != 'כן') {
+        const many = prompt('כמה אורחים תהיו?');
+    } else {
+
+    }
+}
+
+aloF()
