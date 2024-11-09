39aa5b6dc415292749d29cd622ea4aac0aa3b3c9
samuel-f1602 <samuelfrau20@gmail.com>
2024-11-09 20:39:35 +0200

    new

--- שיעור 3/ex1-script.js
+++ שיעור 3/ex1-script.js
@@ -13,3 +13,24 @@ function aloF() {
 }
 
 aloF()
+
+alert('האופציות שיש לי להציע לך הן: אופציה 1 - זוגי מפנק - עולה 200 ש״ח לאדם אופציה 2 - זוגי סוויטה - עולה 300 ש״ח לאדם');
+
+let roomP;
+function rooF() {
+    let room = prompt('באיזה אופציה תהיה מעוניין?');
+
+    if (room === '1') {
+        roomP = 200;
+    } else if (room === '2') {
+        roomP = 300;
+    }
+}
+
+rooF();
+
+let night = prompt('כמה לילות?');
+
+let cost = roomP * many * night;
+
+alert('היי');
