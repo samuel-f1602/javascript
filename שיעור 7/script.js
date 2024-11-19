32b8f30eca401cfaf001c4b05e021f1d5eed2d0d
samuel-f1602 <samuelfrau20@gmail.com>
2024-11-19 19:01:49 +0200

    שיעור 7

---
+++ שיעור 7/script.js
@@ -0,0 +1,142 @@
+console.log("*********hw class***********");
+
+const grades = [80, 85, 40, 100, 60];
+
+function calcStudentGrades() {
+    const failed = [];
+    const passed = [];
+    const excellent = [];
+
+    for (let grade of grades) {
+        if (grade <= 69) {
+            failed.push(grade);
+        } else if (grade >= 70 && grade <= 90) {
+            passed.push(grade);
+        } else {
+            excellent.push(grade);
+        }
+    }
+    console.log('failed:', failed);
+    console.log('passed:', passed);
+    console.log('excellent:', excellent);
+
+    //כדי שיהיה יותר יפה 
+    // console.log(failed.length  + ' students failed');
+
+    //כדי לקבל את הכמות ולא את  הציונים נוסיף
+    //console.log('failed:', failed.length);
+    //console.log('passed:', passed.length);
+    //console.log('excellent:', excellent.length);
+}
+
+calcStudentGrades();
+
+console.log("ֿ\n\n********* filter ********");
+console.log("*********hw class***********");
+
+
+function filterStudentGrades() {
+    const failed = grades.filter(function (someitem) {
+        return someitem <= 69;
+    });
+
+    const passed = grades.filter(function (someitem) {
+        return someitem >= 70 && someitem <= 90;
+    });
+    const excellent = grades.filter(function (someitem) {
+        return someitem >= 90;
+    });
+
+    console.log('failed:', failed.length);
+    console.log('passed:', passed.length);
+    console.log('excellent:', excellent.length);
+}
+
+filterStudentGrades();
+
+
+console.log("\n\n*********ex********");
+//צרו מערך המכיל 5 בעלי חיים
+const animals = ['dog', 'cat', 'fish', 'bird', 'snake'];
+
+//סננו מתוך המערך את כל החיות שאורך שמם גדול מ 3
+//d סננו מתוך המערך את כל החיות שמתחילות באות 
+
+function moreThree() {
+    const above = animals.filter(function (animal) {
+        return animal.length > 3
+    })
+    console.log(above);
+
+    const d = animals.filter(function (item) {
+        return item[0] === 'd';
+    })
+    console.log(d);
+
+}
+
+moreThree();
+
+console.log("\n\n*********foreach********");
+
+const countries = ['israel', 'usa', 'france', 'italy', 'spain',];
+
+// for of איך עושים ב
+//for (let country of countries) {
+//console.log(country);
+//}
+
+// נשים חץ function פונקצית חץ מחליפה את המילה 
+//אחרי המשתנה שבחרנו
+
+//for each בא עם פרמטר נוספים כמו מערך ואינדקס
+//לא חייב להשתמש בפרמטר השלישי שהוא מערך
+
+/*countries.forEach((country, i, arr) => {
+    console.log(i + "- " + country);
+    console.log(arr);
+    }); */
+
+
+//זה בעצם שילוב של for in + for of
+countries.forEach((country, i) => {
+    console.log(i + "- " + country);
+
+});
+
+//0- israel
+//1- usa
+//2- france
+//3- italy
+//4- spain
+
+console.log("\n\n********* map ********");
+//ככה היינו עושים ב foreach
+/* const numbers = [1, 2, 3, 4, 5];
+const numbers2 = [];
+ 
+numbers.forEach(num => {
+    numbers2.push(num * 100);
+    })
+    
+    console.log(numbers2);
+    */
+// ככה נעשה ב map
+// זה מערך חדש מהמערך הקיים עם תוספת שינוי map
+//לדוגמא הכפלה
+const numbers = [1, 2, 3, 4, 5];
+
+const numbers2 = numbers.map((num) => {
+    return num * 100;
+})
+
+console.log(numbers2);
+//אפשר לחבר פילטר עם מאפ
+/* const numbers2 = numbers.filter(
+).map((num) => {
+    return num * 100;
+})
+    */
+
+console.log("\n\n*********ex********");
+
