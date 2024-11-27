508c7aa28454aae19a69ef210bca4fb653838115
samuel-f1602 <samuelfrau20@gmail.com>
2024-11-27 17:55:08 +0200

    flag

--- שיעור 9/date.js
+++ שיעור 9/date.js
@@ -37,10 +37,26 @@ setInterval(() => {
 //console.log(parisTime.toLocaleTimeString());
 
 setInterval(() => {
-    const parisTime = new Date();
-    parisTime.setHours(parisTime.getHours() - 1);
-    const newTime = document.getElementById("pTime");
-    newTime.innerHTML = parisTime.toLocaleTimeString();
+    const franceTime = new Date();
+    franceTime.setHours(franceTime.getHours() - 1);
+    const newTime = document.getElementById("fTime");
+    newTime.innerHTML = franceTime.toLocaleTimeString();
+}, 1000)
+
+
+setInterval(() => {
+    const usaTime = new Date();
+    usaTime.setHours(usaTime.getHours() - 7);
+    const newTime = document.getElementById("uTime");
+    newTime.innerHTML = usaTime.toLocaleTimeString();
+}, 1000)
+
+
+setInterval(() => {
+    const englendTime = new Date();
+    englendTime.setHours(englendTime.getHours() - 2);
+    const newTime = document.getElementById("eTime");
+    newTime.innerHTML = englendTime.toLocaleTimeString();
 }, 1000)
 
 
@@ -52,10 +68,34 @@ setInterval(() => {
 }, 1000)
 
 setInterval(() => {
+    const italyTime = new Date();
+    italyTime.setHours(italyTime.getHours() - 1);
+    const el = document.getElementById("itTime");
+    el.innerHTML = italyTime.toLocaleTimeString();
+}, 1000)
+
+
+setInterval(() => {
+    const chinaTime = new Date();
+    chinaTime.setHours(chinaTime.getHours() + 6);
+    const el = document.getElementById("cTime");
+    el.innerHTML = chinaTime.toLocaleTimeString();
+}, 1000)
+
+
+setInterval(() => {
     const mexicoTime = new Date();
     mexicoTime.setHours(mexicoTime.getHours() - 7);
     const el = document.getElementById("mTime");
     el.innerHTML = mexicoTime.toLocaleTimeString();
+}, 1000)
+
+
+setInterval(() => {
+    const brazilTime = new Date();
+    brazilTime.setHours(brazilTime.getHours() - 5);
+    const el = document.getElementById("bTime");
+    el.innerHTML = brazilTime.toLocaleTimeString();
 }, 1000)
 
 
